0d183f98a9e925232338fb30129a21084867c9d0
unknown <nataligenius1@gmail.com>
2025-10-12 22:24:39 +0300

    aaa

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ if st.session_state.criterias:
           st.write(f"Відсортуйте підкритерії критерію {c}")
           sorting2 = st.multiselect(
               label="(перший - найважливіший)",
-              options=subcrs, default=subcrs, key="s2")
+              options=subcrs, default=subcrs, key=f"s2-{subcr}")
           st.session_state.criterias[c] = sorting2
 
 # бек ========================================
